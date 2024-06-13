13a4a6e01150d882f99746cd6875a2edb91ae29a
km4.kim <kyoomok@naver.com>
2024-06-13 09:54:34 +0900

    [feature]45

--- prime_factors.py
+++ prime_factors.py
@@ -13,6 +13,9 @@ class PrimeFactor:
                         factors.append(divisor)
                         num /= divisor
                     divisor += 1
+            elif num == 9:
+                factors.append(3)
+                factors.append(3)
             else:
                 factors.append(num)
         return factors
